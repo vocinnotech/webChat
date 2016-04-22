12f7e6ced97f7a1aa2a3da6abfbe50cbbf55771d
jason <jason@vocinno.com>
2016-04-22 11:32:31 +0800

    proj init

---
+++ index.js
@@ -0,0 +1,4 @@
+require("babel/register")({
+  optional: ["es7.comprehensions"]
+});
+require("./lib/app.js");
---
+++ localrun.sh
@@ -0,0 +1,20 @@
+export MONGO_URL=mongodb://123.59.81.219:27017/laohaowan
+export MEMCACHED_URL="123.59.81.219:11211"
+export NEWRELIC_APP_NAME=laohaowan
+export SERVER_TOKEN=woingi929k48fj2372he8dn289kmoi3
+export RABBIT_URL="amqp://rabbit.vitamin.bengjiujie.com"
+export FILE_DOMAIN="file.bengjiujie.com"
+export REDIS_URL="redis://123.59.81.219:6379"
+export THIRDPARTY_INNER_SERVER=http://thirdpart.laohaowan.vitamin.bengjiujie.com
+# export THIRDPARTY_INNER_SERVER=http://localhost:5001
+export NEW_RELIC_KEY=67bf4669104d1a19a40790058be77e7abb95f129
+
+export VOICE_DOMAIN="http://file.laohaowan.vitamin.bengjiujie.com/voice/laohaowan"
+export IMAGE_DOMAIN="http://file.laohaowan.vitamin.bengjiujie.com/image/laohaowan"
+export RADIO_SERVICE="http://radio.laohaowan.vitamin.bengjiujie.com"
+export MATERIAL_SERVICE="http://material.laohaowan.vitamin.bengjiujie.com"
+export CHAT_SERVICE="http://im.laohaowan.vitamin.bengjiujie.com"
+export USERACTION_SERVICE="http://useraction.laohaowan.vitamin.bengjiujie.com"
+export THIRDPART_SERVICE="http://thirdparty.laohaowan.vitamin.bengjiujie.com"
+export FILE_SERVICE="http://file.laohaowan.vitamin.bengjiujie.com"
+npm start
